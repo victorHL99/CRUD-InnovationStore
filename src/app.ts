dec6537e400c462053135b6cc9e2d2f4ea757e67
victorHL99 <victor.lessa@estudante.ufla.br>
2023-01-16 16:05:34 -0300

    feat: :sparkles: create handler error

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import cors from 'cors';
+import 'express-async-errors';
+
+import errorHandlerMiddleware from './middlewares/errorHandlerMiddleware';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(errorHandlerMiddleware);
+
 
 export default app;
