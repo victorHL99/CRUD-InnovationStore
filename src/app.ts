90a96df69bb1ac7ade396c15ca3c4c1da168f2f7
victorHL99 <victor.lessa@estudante.ufla.br>
2023-01-16 16:12:46 -0300

    feat: :sparkles: added routes for app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import cors from 'cors';
 import 'express-async-errors';
 
 import errorHandlerMiddleware from './middlewares/errorHandlerMiddleware';
+import routes from './routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(routes)
 app.use(errorHandlerMiddleware);
 
 
