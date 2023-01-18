a6e50c2405f6742abacefd227bbfce75b36e0e6c
victorHL99 <victor.lessa@estudante.ufla.br>
2023-01-18 15:08:25 -0300

    feat: :sparkles: create service for get all products

--- src/repositories/productsRepository.ts
+++ src/repositories/productsRepository.ts
@@ -7,8 +7,15 @@ async function createProduct(product: CreateProduct) {
   })
 }
 
+async function getProducts() {
+  const products = await client.products.findMany()
+
+  return products
+}
+
 const productsRepository = {
-  create: createProduct
+  create: createProduct,
+  read: getProducts
 }
 
 export default productsRepository;
