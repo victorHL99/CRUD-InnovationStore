a00ba43d6e4a62f7316525f4b3e7f82f41c63117
victorHL99 <victor.lessa@estudante.ufla.br>
2023-01-18 15:07:52 -0300

    feat: :sparkles: function for return all products

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -17,8 +17,15 @@ async function createProduct(req: Request, res: Response) {
   return res.status(201).json({ message: "Product created" })
 }
 
+async function getProducts(req: Request, res: Response) {
+  const products = await productsService.read()
+
+  return res.status(200).json(products)
+}
+
 const productsController = {
-  create: createProduct
+  create: createProduct,
+  getProducts
 }
 
 export default productsController;
