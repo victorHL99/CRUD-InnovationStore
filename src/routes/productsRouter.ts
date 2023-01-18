13706c02880da023fbf36cb984eca19e136d1b04
victorHL99 <victor.lessa@estudante.ufla.br>
2023-01-18 15:33:44 -0300

    feat: :sparkles: create route for delete product

--- src/routes/productsRouter.ts
+++ src/routes/productsRouter.ts
@@ -8,5 +8,5 @@ const productsRouter = Router();
 productsRouter.post("/", validateSchema(productSchema.createProduct), productsController.create)
 productsRouter.get("/", productsController.getProducts)
 productsRouter.put("/:id", validateSchema(productSchema.createProduct), productsController.update)
-
+productsRouter.delete("/:id", productsController.delete)
 export default productsRouter;
