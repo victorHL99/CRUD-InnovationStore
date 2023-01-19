02891fe5b0fdfc7700d04605247ba1ff3488ddc3
victorHL99 <victor.lessa@estudante.ufla.br>
2023-01-19 14:21:57 -0300

    feat: :sparkles: create route for find cep

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import productsRouter from "./productsRouter";
+import cepRouter from "./cepRouter";
 
 const routes = Router();
 
 // routes.use(authRouter);
 routes.use("/products", productsRouter);
+routes.use("/cep", cepRouter);
 
 export default routes;
